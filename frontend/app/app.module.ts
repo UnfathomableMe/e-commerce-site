2f80a53a89909d9ae82c4a1b1fb2a89c45d470f5
UnfathomableMe <54766093+UnfathomableMe@users.noreply.github.com>
2020-07-11 11:19:38 +0530

    Add files via upload

---
+++ frontend/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { MyCartComponent } from './my-cart/my-cart.component';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomepageComponent,
+    ProductDetailsComponent,
+    LoginComponent,
+    RegisterComponent,
+    MyCartComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
